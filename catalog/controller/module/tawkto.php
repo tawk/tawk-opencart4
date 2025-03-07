50dcb7bd930fb491113574b9cdaf09a226fac459
eug-L <eugene4198@gmail.com>
2025-03-07 13:46:06 +0800

    add hashing for visitor recognition

--- catalog/controller/module/tawkto.php
+++ catalog/controller/module/tawkto.php
@@ -31,13 +31,17 @@ class Tawkto extends Controller
 	 */
 	public function index()
 	{
+		if (session_status() === PHP_SESSION_NONE && !headers_sent()) {
+			session_start();
+		}
+
 		$this->load->model('setting/setting');
 
 		$data = array();
-		$data['visitor'] = $this->getVisitor();
 
 		$privacy_opts = $this->config->get('tawkto_privacy');
 		$cart_opts = $this->config->get('tawkto_cart');
+		$security_opts = $this->config->get('tawkto_security');
 
 		$settings = $this->getCurrentSettings();
 		if (isset($settings['module_tawkto_privacy'])) {
@@ -46,8 +50,15 @@ class Tawkto extends Controller
 		if (isset($settings['module_tawkto_cart'])) {
 			$cart_opts = $settings['module_tawkto_cart'];
 		}
+		if (isset($settings['module_tawkto_security'])) {
+			$security_opts = $settings['module_tawkto_security'];
+		}
 
-		$data['enable_visitor_recognition'] = $privacy_opts['enable_visitor_recognition'];
+		$data['visitor'] = $this->getVisitor(array(
+			'enable_visitor_recognition' => $privacy_opts['enable_visitor_recognition'],
+			'secure_mode_enabled' => $security_opts['secure_mode_enabled'],
+			'js_api_key' => $security_opts['js_api_key'],
+		));
 		$data['can_monitor_customer_cart'] = $cart_opts['monitor_customer_cart'];
 
 		$widget = $this->getWidget();
@@ -143,16 +154,36 @@ class Tawkto extends Controller
 	/**
 	 * Get visitor details
 	 *
+	 * @param array $params
 	 * @return string|null
 	 */
-	private function getVisitor()
+	private function getVisitor($params)
 	{
+		$enable_visitor_recognition = $params['enable_visitor_recognition'];
+		$secure_mode_enabled = $params['secure_mode_enabled'];
+		$encrypted_js_api_key = $params['js_api_key'];
+
+		if (!$enable_visitor_recognition) {
+			return null;
+		}
+
 		$logged_in = $this->customer->isLogged();
 		if ($logged_in) {
 			$data = array(
 					'name' => $this->customer->getFirstName().' '.$this->customer->getLastName(),
 					'email' => $this->customer->getEmail(),
 				);
+
+			if ($secure_mode_enabled && !is_null($encrypted_js_api_key)) {
+				try {
+					$js_api_key = $this->getJsApiKey($encrypted_js_api_key);
+				} catch (\Exception $e) {
+					return null;
+				}
+
+				$data['hash'] = hash_hmac('sha256', $this->customer->getEmail(), $js_api_key);
+			}
+
 			return json_encode($data);
 		}
 
@@ -168,5 +199,60 @@ class Tawkto extends Controller
 	{
 		$store_id = $this->config->get('config_store_id');
 		return $this->model_setting_setting->getSetting('module_tawkto', $store_id);
+	}
+
+	/**
+	 * Get js_api_key
+	 * @param string $encrypted_js_api_key
+	 * @return string JS API key
+	 */
+	private function getJsApiKey($encrypted_js_api_key)
+	{
+		if (isset($_SESSION['tawkto_js_api_key'])) {
+			return $_SESSION['tawkto_js_api_key'];
+		}
+
+		$js_api_key = $this->decryptData($encrypted_js_api_key);
+
+		$_SESSION['tawkto_js_api_key'] = $js_api_key;
+
+		return $js_api_key;
+	}
+
+	/**
+	 * Decrypt data
+	 * @param mixed $data
+	 * @return string Decrypted data
+	 */
+	private function decryptData($data)
+	{
+		$filePath = DIR_EXTENSION . 'tawkto/system/config/credentials.json';
+
+		if (!file_exists($filePath)) {
+			throw new \Exception('Credentials file not found');
+		}
+
+		$credentials = json_decode(file_get_contents($filePath), true);
+
+		if (!isset($credentials['encryption_key'])) {
+			throw new \Exception('Encryption key not found');
+		}
+
+		$decoded = base64_decode($data);
+
+		if ($decoded === false) {
+			throw new \Exception('Failed to decode data');
+		}
+
+		$iv = substr($decoded, 0, 16);
+		$encrypted_data = substr($decoded, 16);
+
+		$decrypted_data = openssl_decrypt($encrypted_data, 'AES-256-CBC', $credentials['encryption_key'], 0, $iv);
+
+		if ($decrypted_data === false) {
+			throw new \Exception('Failed to decrypt data');
+		}
+
+		return $decrypted_data;
 	}
 }
